be24934ce93533a59bb8780b0cd7f0f70d4307c3
esrakubra <esra96elden@gmail.com>
2023-01-19 14:55:35 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+//ELSE IF YAPISI ILE YOL AYRIMI UYGULAMASI
+
+
+
+let secilenYol = prompt ("lütfen gitmek istediğiniz yolu seçiniz:");
+
+if(secilenYol ==1){
+    alert("Seçilen yol "+ secilenYol+".yoldur.");
+}
+else if(ecilenYol ==2){
+    alert("Seçilen yol"+ secilenYol +".yoldur.");
+}
+else if(secilenYol ==3){
+    alert("Seçilen yol"+ secilenYol +".yoldur.");
+}
+else{
+    alert("Lütfen geçerli bir yol seçiniz !!");
+}
